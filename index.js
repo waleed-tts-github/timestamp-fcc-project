70d94b5d8f9f0e7015e17172b8d7a6e822ea76ee
waleed bukhari <muhammadwaleed872.tts@gmail.com>
2025-01-13 10:51:53 +0000

    Completed timestamp microservices project

--- index.js
+++ index.js
@@ -2,31 +2,54 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/api/:date", (req, res) => {
+  let date = req.params.date;
+  //if i am getting date in unix format, then i need to convert into number. Because by default params come in string format
+  if (!isNaN(date)) {
+    date = Number(date);
+  }
+  let unix = new Date(date).getTime();
+  let utc = new Date(date).toUTCString();
+  const timestamp = Number(unix);
+  // Check if the timestamp is within the valid range for seconds
+  if (!(timestamp > 0 && timestamp < 253402300799999) || isNaN(unix)) {
+    return res.json({
+      error: "Invalid Date",
+    });
+  }
 
-// your first API endpoint... 
+  return res.json({ unix, utc });
+});
+
+app.get("/api/", (req, res) => {
+  return res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+  });
+});
+
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
-
-
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
